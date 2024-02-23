6d8cbf9fbe21943d1a25fdfba7ccdb6eaf48029e
Blajan George <george.blajan@sentiocx.com>
2024-02-23 16:12:16 +0200

    refactor

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -30,49 +31,50 @@ func main() {
 
 		go func(consumerName string) {
 			defer wg.Done()
-
 			rdb := redis.NewClient(&redis.Options{
 				Addr:     fmt.Sprintf("%s:%d", host, port),
 				Password: pass,
 				DB:       db,
 			})
 			fmt.Printf("%s start\n", consumerName)
-			initialMessage := rdb.XRead(ctx, &redis.XReadArgs{Streams: []string{key, "$"}, Count: 1, Block: 0})
-
 			for {
-				cursorId := initialMessage.Val()[0].Messages[0].ID
-				streams := rdb.XRead(ctx, &redis.XReadArgs{Streams: []string{key, cursorId}, Count: 1, Block: 0})
-				for _, message := range streams.Val()[0].Messages {
-					for key := range message.Values {
-						recordTime, _ := strconv.ParseInt(key, 10, 64)
-						//fmt.Printf("%s\n", strconv.FormatInt(recordTime, 10))
-						//fmt.Printf("%s\n", strconv.FormatInt(time.Now().UnixMilli(), 10))
-						fmt.Printf("latency: %s\n", strconv.FormatInt(time.Now().UnixMilli()-recordTime, 10))
-						//time.Sleep(5 * time.Second)
-					}
+				var cursorId string
+				var currentMessage []redis.XStream
+				if cursorId == "" {
+					currentMessage, _ = rdb.XRead(ctx, &redis.XReadArgs{Streams: []string{key, "$"}, Block: 0, Count: 1}).Result()
+				} else {
+					currentMessage, _ = rdb.XRead(ctx, &redis.XReadArgs{Streams: []string{key, cursorId}, Block: 0, Count: 1}).Result()
+				}
+				cursorId = currentMessage[0].Messages[0].ID
+				for key := range currentMessage[0].Messages[0].Values {
+					recordTime, _ := strconv.ParseInt(key, 10, 64)
+					toRedisTime, _ := strconv.ParseInt(strings.Split(cursorId, "-")[0], 10, 64)
+					//fmt.Printf("%s\n", strconv.FormatInt(recordTime, 10))
+					//fmt.Printf("%s\n", strconv.FormatInt(time.Now().UnixMilli(), 10))
+					fmt.Printf("latency to consumer: %s\n", strconv.FormatInt(time.Now().UnixMilli()-recordTime, 10))
+					fmt.Printf("latency to redis: %s\n", strconv.FormatInt(time.Now().UnixMilli()-toRedisTime, 10))
 				}
 			}
-
 		}(fmt.Sprintf("consumer-%d", i))
 	}
+
+	time.Sleep(1 * time.Second)
 
 	for i := 0; i < producers; i++ {
 		wg.Add(1)
 
 		go func(producerName string) {
 			defer wg.Done()
-
 			rdb := redis.NewClient(&redis.Options{
 				Addr:     fmt.Sprintf("%s:%d", host, port),
 				Password: pass,
 				DB:       db,
 			})
 			fmt.Printf("%s start\n", producerName)
-
 			for {
 				rdb.XAdd(ctx, &redis.XAddArgs{Stream: key, Values: []interface{}{time.Now().UnixMilli(), ""}})
+				time.Sleep(2 * time.Second)
 			}
-
 		}(fmt.Sprintf("producer-%d", i))
 	}
 
